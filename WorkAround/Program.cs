a9c930c20cee200b1eb4f177a497427da37799d6
demet <demet@MOZ-DYN>
2023-11-28 19:58:05 +0300

    Add project files.

---
+++ WorkAround/Program.cs
@@ -0,0 +1,11 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+Person person1 = new Person();
+person1.FirstName = "Demet";
+person1.LastName = "Özmen";
+person1.DateOfBirthYear = 1992;
+person1.NationalIdentity = 12713201322;
+
+PttManager pttManager = new PttManager(new PersonManager());
+pttManager.GiveMask(person1);
